aa9a46c5192a9271508ad6e4bdef46775ebbb8ac
Albert <alteos98@gmail.com>
2019-03-04 17:13:28 +0100

    modificación para los niveles

--- 2DGame/2DGame/2DGame/TileMap.cpp
+++ 2DGame/2DGame/2DGame/TileMap.cpp
@@ -83,10 +83,15 @@ bool TileMap::loadLevel(const string &levelFile)
 		for(int i=0; i<mapSize.x; i++)
 		{
 			fin.get(tile);
-			if(tile == ' ')
-				map[j*mapSize.x+i] = 0;
-			else
-				map[j*mapSize.x+i] = tile - int('0');
+			while (tile == ',') {
+				fin.get(tile);
+			}
+			int aux = 0;
+			while (tile != ',' && tile != '.') {
+				aux = tile - int('0') + aux * 10;
+				fin.get(tile);
+			}
+			map[j*mapSize.x+i] = aux;
 		}
 		fin.get(tile);
 #ifndef _WIN32
@@ -115,7 +120,7 @@ void TileMap::prepareArrays(const glm::vec2 &minCoords, ShaderProgram &program)
 				// Non-empty tile
 				nTiles++;
 				posTile = glm::vec2(minCoords.x + i * tileSize, minCoords.y + j * tileSize);
-				texCoordTile[0] = glm::vec2(float((tile-1)%2) / tilesheetSize.x, float((tile-1)/2) / tilesheetSize.y);
+				texCoordTile[0] = glm::vec2(float((tile-1)%tilesheetSize.x) / tilesheetSize.x, float((tile-1)/tilesheetSize.x) / tilesheetSize.y);
 				texCoordTile[1] = texCoordTile[0] + tileTexSize;
 				//texCoordTile[0] += halfTexel;
 				texCoordTile[1] -= halfTexel;
