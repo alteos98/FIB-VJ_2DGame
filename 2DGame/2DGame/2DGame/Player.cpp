e99975f9f9b4223926590d9a5c61741aa10b3b6f
Albert <alteos98@gmail.com>
2019-03-06 18:43:57 +0100

    ya funcionan las animaciones

--- 2DGame/2DGame/2DGame/Player.cpp
+++ 2DGame/2DGame/2DGame/Player.cpp
@@ -13,7 +13,6 @@
 
 enum PlayerAnims
 {
-	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT,
 	STAND_LEFT_DOWN, STAND_RIGHT_DOWN, MOVE_LEFT_DOWN, MOVE_RIGHT_DOWN,
 	STAND_LEFT_UP, STAND_RIGHT_UP, MOVE_LEFT_UP, MOVE_RIGHT_UP
 };
@@ -25,21 +24,35 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 
 	spritesheet.loadFromFile("images/Player.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(1.f/6.f, 1.f/6.f), &spritesheet, &shaderProgram);
-	sprite->setNumberAnimations(4);
+	sprite->setNumberAnimations(8);
 
-	sprite->setAnimationSpeed(STAND_LEFT, 8);
-	sprite->addKeyframe(STAND_LEFT, glm::vec2(3.f / 6.f, 2.f / 6.f));
+	sprite->setAnimationSpeed(STAND_LEFT_DOWN, 8);
+	sprite->addKeyframe(STAND_LEFT_DOWN, glm::vec2(3.f / 6.f, 2.f / 6.f));
 
-	sprite->setAnimationSpeed(STAND_RIGHT, 8);
-	sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.f, 0.f));
+	sprite->setAnimationSpeed(STAND_RIGHT_DOWN, 8);
+	sprite->addKeyframe(STAND_RIGHT_DOWN, glm::vec2(0.f, 0.f));
 
-	sprite->setAnimationSpeed(MOVE_LEFT, 8);
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(3.f / 6.f, 2.f / 6.f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(2.f / 6.f, 2.f / 6.f));
+	sprite->setAnimationSpeed(MOVE_LEFT_DOWN, 8);
+	sprite->addKeyframe(MOVE_LEFT_DOWN, glm::vec2(3.f / 6.f, 2.f / 6.f));
+	sprite->addKeyframe(MOVE_LEFT_DOWN, glm::vec2(2.f / 6.f, 2.f / 6.f));
 
-	sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.f, 0.f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(1.f / 6.f, 0.f));
+	sprite->setAnimationSpeed(MOVE_RIGHT_DOWN, 8);
+	sprite->addKeyframe(MOVE_RIGHT_DOWN, glm::vec2(0.f, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT_DOWN, glm::vec2(1.f / 6.f, 0.f));
+
+	sprite->setAnimationSpeed(STAND_LEFT_UP, 8);
+	sprite->addKeyframe(STAND_LEFT_UP, glm::vec2(3.f / 6.f, 1.f / 6.f));
+
+	sprite->setAnimationSpeed(STAND_RIGHT_UP, 8);
+	sprite->addKeyframe(STAND_RIGHT_UP, glm::vec2(0.f, 3.f / 6.f));
+
+	sprite->setAnimationSpeed(MOVE_LEFT_UP, 8);
+	sprite->addKeyframe(MOVE_LEFT_UP, glm::vec2(3.f / 6.f, 1.f / 6.f));
+	sprite->addKeyframe(MOVE_LEFT_UP, glm::vec2(2.f / 6.f, 1.f / 6.f));
+
+	sprite->setAnimationSpeed(MOVE_RIGHT_UP, 8);
+	sprite->addKeyframe(MOVE_RIGHT_UP, glm::vec2(0.f, 3.f / 6.f));
+	sprite->addKeyframe(MOVE_RIGHT_UP, glm::vec2(1.f / 6.f, 3.f / 6.f));
 
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
@@ -47,48 +60,77 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 
 }
 
-void Player::update(int deltaTime)
-{
+void Player::update(int deltaTime) {
 	sprite->update(deltaTime);
-	if(Game::instance().getSpecialKey(GLUT_KEY_LEFT))
-	{
-		if(sprite->animation() != MOVE_LEFT)
-			sprite->changeAnimation(MOVE_LEFT);
-		posPlayer.x -= 2;
-		if(map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
-		{
-			posPlayer.x += 2;
-			sprite->changeAnimation(STAND_LEFT);
+	if(Game::instance().getSpecialKey(GLUT_KEY_LEFT)) {
+		if (isOnFloor) {
+			if (sprite->animation() != MOVE_LEFT_DOWN)
+				sprite->changeAnimation(MOVE_LEFT_DOWN);
+			posPlayer.x -= 2;
+			if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
+			{
+				posPlayer.x += 2;
+				sprite->changeAnimation(STAND_LEFT_DOWN);
+			}
+		}
+		else {
+			if (sprite->animation() != MOVE_LEFT_UP)
+				sprite->changeAnimation(MOVE_LEFT_UP);
+			posPlayer.x -= 2;
+			if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)))
+			{
+				posPlayer.x += 2;
+				sprite->changeAnimation(STAND_LEFT_UP);
+			}
 		}
 	}
-	else if(Game::instance().getSpecialKey(GLUT_KEY_RIGHT))
-	{
-		if(sprite->animation() != MOVE_RIGHT)
-			sprite->changeAnimation(MOVE_RIGHT);
-		posPlayer.x += 2;
-		if(map->collisionMoveRight(posPlayer, glm::ivec2(32, 32)))
-		{
-			posPlayer.x -= 2;
-			sprite->changeAnimation(STAND_RIGHT);
+	else if(Game::instance().getSpecialKey(GLUT_KEY_RIGHT)) {
+		if (isOnFloor) {
+			if (sprite->animation() != MOVE_RIGHT_DOWN)
+				sprite->changeAnimation(MOVE_RIGHT_DOWN);
+			posPlayer.x += 2;
+			if (map->collisionMoveRight(posPlayer, glm::ivec2(32, 32)))
+			{
+				posPlayer.x -= 2;
+				sprite->changeAnimation(STAND_RIGHT_DOWN);
+			}
+		}
+		else {
+			if (sprite->animation() != MOVE_RIGHT_UP)
+				sprite->changeAnimation(MOVE_RIGHT_UP);
+			posPlayer.x += 2;
+			if (map->collisionMoveRight(posPlayer, glm::ivec2(32, 32)))
+			{
+				posPlayer.x -= 2;
+				sprite->changeAnimation(STAND_RIGHT_UP);
+			}
 		}
 	}
 	else
 	{
-		if(sprite->animation() == MOVE_LEFT)
-			sprite->changeAnimation(STAND_LEFT);
-		else if(sprite->animation() == MOVE_RIGHT)
-			sprite->changeAnimation(STAND_RIGHT);
+		if (isOnFloor) {
+			if (sprite->animation() == MOVE_LEFT_DOWN)
+				sprite->changeAnimation(STAND_LEFT_DOWN);
+			else if (sprite->animation() == MOVE_RIGHT_DOWN)
+				sprite->changeAnimation(STAND_RIGHT_DOWN);
+		}
+		else {
+			if (sprite->animation() == MOVE_LEFT_UP)
+				sprite->changeAnimation(STAND_LEFT_UP);
+			else if (sprite->animation() == MOVE_RIGHT_UP)
+				sprite->changeAnimation(STAND_RIGHT_UP);
+		}
 	}
 
 	if (bGravity) {
-		if (!isOnFloor) { // roof
+		if (!isOnFloor) {
 			posPlayer.y += FALL_STEP;
 			if (map->collisionMoveDown(posPlayer, glm::ivec2(32, 32), &posPlayer.y, FALL_STEP)) {
 				bGravity = false;
 				isOnFloor = true;
 			}
 		}
-		else { // floor
+		else {
 			posPlayer.y -= FALL_STEP;
 			if (map->collisionMoveUp(posPlayer, glm::ivec2(32, 32), &posPlayer.y, FALL_STEP)) {
 				bGravity = false;
@@ -97,16 +139,24 @@ void Player::update(int deltaTime)
 		}
 	}
 	else {
-		if (!isOnFloor) { // roof
+		if (!isOnFloor) {
 			posPlayer.y -= FALL_STEP;
+			if (sprite->animation() == STAND_LEFT_DOWN)
+				sprite->changeAnimation(STAND_LEFT_UP);
+			else if (sprite->animation() == STAND_RIGHT_DOWN)
+				sprite->changeAnimation(STAND_RIGHT_UP);
 			if (map->collisionMoveUp(posPlayer, glm::ivec2(32, 32), &posPlayer.y, FALL_STEP)) {
 				if (Game::instance().getSpecialKey(GLUT_KEY_DOWN)) {
 					bGravity = true;
 				}
 			}
 		}
-		else { // floor
+		else {
 			posPlayer.y += FALL_STEP;
+			if (sprite->animation() == STAND_LEFT_UP)
+				sprite->changeAnimation(STAND_LEFT_DOWN);
+			else if (sprite->animation() == STAND_RIGHT_UP)
+				sprite->changeAnimation(STAND_RIGHT_DOWN);
 			if (map->collisionMoveDown(posPlayer, glm::ivec2(32, 32), &posPlayer.y, FALL_STEP)) {
 				if (Game::instance().getSpecialKey(GLUT_KEY_DOWN)) {
 					bGravity = true;
