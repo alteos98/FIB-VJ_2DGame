756c410261a2d8c6946f81051e97b96a85d4170b
Retrater12 <43724195+retrater12@users.noreply.github.com>
2019-03-13 08:28:16 +0100

    Update Scene.cpp

--- 2DGame/2DGame/2DGame/Scene.cpp
+++ 2DGame/2DGame/2DGame/Scene.cpp
@@ -102,10 +102,11 @@ void Scene::initShaders()
 bool Scene::ButtonPress(int x, int y) {
 	if ((x > button->getposB().x + SCREEN_X && x < (button->getposB().x + button->getWidth() + SCREEN_X))
 		&& (y > button->getposB().y + SCREEN_Y && y < (button->getposB().y + button->getHeight() + SCREEN_Y))) {
-		printf("¡GO!");
+		printf("¡GO!");printf("%i, %i \n", x, y);
+		return true;
 	}
 	printf("%i, %i \n", x, y);
-	return true;
+	return false;
 }
 
 
